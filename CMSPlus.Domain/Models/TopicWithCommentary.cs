41fae696f4507769b9c539c92b06e132a6a03199
lenapapuc <elenapapuc99@gmail.com>
2025-01-19 13:13:27 +0200

    Added the models for the replies, need to correct them later. Added the button for creation of commentaries in the details page

---
+++ CMSPlus.Domain/Models/TopicWithCommentary.cs
@@ -0,0 +1,8 @@
+﻿namespace CMSPlus.Domain.Models
+{
+    public class TopicWithCommentaryCreateModel
+    {
+        public TopicModels.TopicDetailsModel TopicDetails { get; set; }
+        public CommentaryModels.CommentaryCreateModel CommentaryCreate { get; set; }
+    }
+}
--- CMSPlus.Presentation/Controllers/CommentaryContoller.cs
+++ CMSPlus.Presentation/Controllers/CommentaryContoller.cs
@@ -6,6 +6,9 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using CMSPlus.Services.Services;
+using Microsoft.AspNetCore.Routing;
+using CMSPlus.Domain.Models.TopicModels;
+using CMSPlus.Domain.Models;
 
 namespace CMSPlus.Presentation.Controllers;
 
@@ -19,12 +22,28 @@ public class CommentaryController : Controller
     {
         _commentaryService = commentaryService;
         _mapper = mapper;
-     
+
     }
 
+    public async Task<IActionResult> Index(int topicId)
+    {
+        var commentaries = await _commentaryService.GetByTopicId(topicId);
+        var commentaryToDisplay = _mapper.Map<IEnumerable<CommentaryEntity>, IEnumerable<CommentaryModel>>(commentaries);
+        return View(commentaryToDisplay);
+    }
+
+    [HttpGet]
+    public IActionResult Create(int topicId)
+    {
+        var model = new CommentaryCreateModel
+        {
+            TopicId = topicId // Initialize the model with the TopicId
+        };
+        return View(model);
+    }
 
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CommentaryCreateModel commentary)
+    public async Task<IActionResult> Create(CommentaryCreateModel commentary)
     {
         //var validationResult = await _createModelValidator.ValidateAsync(topic);
         /*if (!validationResult.IsValid)
@@ -34,7 +53,7 @@ public class CommentaryController : Controller
         }*/
         var commentaryEntity = _mapper.Map<CommentaryCreateModel, CommentaryEntity>(commentary);
         await _commentaryService.Create(commentaryEntity);
-        return Ok(commentaryEntity);
+        return RedirectToAction("Index", new { topicId = commentary.TopicId });
     }
 
 
